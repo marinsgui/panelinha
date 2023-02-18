d30896f60d17b5ebd98fdf65075f75eb08bab9eb
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-18 11:06:35 -0300

    pagina home inicial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,17 @@
+import useFetch from '../../hooks/useFetch'
+
 import style from '../../styles/Home.module.css'
 
 export default function Home() {
+    const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+
     return (
-        <div>Home</div>
+        <main className={style.home}>
+            {error && <p className={style.error}>{error}</p>}
+            {isPending && <p className={style.loading}>Loading...</p>}
+            {data && data.map(recipe => (
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
+        </main>
     )
 }
