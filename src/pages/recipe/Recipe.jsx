998919c592cffe57f3583d7ca2631d1d38812cc9
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-18 11:57:19 -0300

    pagina de detalhes criada

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,7 +1,19 @@
+import { useParams } from 'react-router-dom'
+
 import style from '../../styles/Recipe.module.css'
 
+import useFetch from '../../hooks/useFetch'
+
 export default function Recipe() {
+    const {id} = useParams()
+
+    const { data, isPending, error } = useFetch(`http://localhost:3000/recipes/${id}`)
+
     return (
-        <div>Recipe</div>
+        <main className={style.recipe}>
+            {isPending && <p className={style.loading}>Loading...</p>}
+            {error && <p className={style.error}>{error}</p>}
+            {data && <h1>{data.title}</h1>}
+        </main>
     )
 }
