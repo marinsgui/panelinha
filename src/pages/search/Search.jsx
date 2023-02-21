57da9483ce5deaf75436ab60d015f2947e0403ad
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-21 11:01:35 -0300

    funcionalidade de busca criada

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,7 +1,25 @@
+import { useLocation } from 'react-router-dom'
+
 import style from '../../styles/Search.module.css'
 
+import useFetch from '../../hooks/useFetch'
+
+import RecipeList from '../../components/RecipeList'
+
 export default function Search() {
+    const queryString = useLocation().search
+    const queryParams = new URLSearchParams(queryString)
+    const query = queryParams.get('q')
+
+    const url = 'http://localhost:3000/recipes?q=' + query
+    const { error, data, isPending } = useFetch(url)
+
     return (
-        <div>Search</div>
+        <div>
+            <h2 className={style.page_title}>Receitas com "{query}"</h2>
+            {error && <p className={style.error}>{error}</p>}
+            {isPending && <p className={style.loading}>Loading...</p>}
+            {data && <RecipeList recipes={data} />}
+        </div>
     )
 }
