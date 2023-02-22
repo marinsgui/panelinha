774b3701851955e77fb5832ca96bbaaa7e5c78ab
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-22 11:56:31 -0300

    contexto adicionado na navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+
+import { ThemeContext } from '../context/ThemeContext'
 
 import style from '../styles/Navbar.module.css'
 
 import Searchbar from './SearchBar'
 
 export default function Navbar() {
+    const { color } = useContext(ThemeContext)
+
     return (
-        <header className={style.navbar}>
+        <header className={style.navbar} style={{ backgroundColor: color }}>
             <nav>
                 <Link to='/' className={style.link}>
                     <h1>Panelinha</h1>
