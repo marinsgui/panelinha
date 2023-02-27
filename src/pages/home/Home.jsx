c0bb95b0e779589a18c0ab1f412a8a68b01a43ba
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-27 11:43:01 -0300

    acessando dados do banco de dados firestore

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react'
+import { projectFirestore } from '../../firebase/config'
+
 import RecipeList from '../../components/RecipeList'
-import useFetch from '../../hooks/useFetch'
 
 import style from '../../styles/Home.module.css'
 
 export default function Home() {
-    const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(false)
+    
+    useEffect(() => {
+        setIsPending(true)
+
+        projectFirestore.collection('recipes').get().then((snapshot) => {
+            if(snapshot.empty) {
+                setError('Sem receitas para carregar')
+                setIsPending(false)
+            } else {
+                let results = []
+                snapshot.docs.forEach(doc => {
+                    results.push({ id: doc.id, ...doc.data() })
+                })
+                setData(results)
+                setIsPending(false)
+            }
+        }).catch(err => {
+            setError(err.message)
+            setIsPending(false)
+        })
+    }, [])
 
     return (
         <main className={style.home}>
